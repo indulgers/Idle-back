7e287b0b850e93ac839ab639256a41edb1932350
indulgers <1370356809@qq.com>
2025-02-17 21:28:44 +0800

    feat: swagger doc

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
+  app.setGlobalPrefix('api/main');
+  const config = new DocumentBuilder()
+    .setTitle('Main server')
+    .setDescription('The API for managing main server')
+    .setVersion('1.0')
+    .addTag('main')
+    .addBearerAuth({
+      type: 'http',
+      description: 'Enter JWT token in format **Bearer** <JWT>',
+    })
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/main/doc', app, documentFactory);
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
