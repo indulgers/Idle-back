02f6dad6d7de7db696d783e31eeba8430e5f23ae
indulgers <1370356809@qq.com>
2025-02-17 13:34:16 +0800

    feat: main deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,5 +24,17 @@ module.exports = {
         PORT: 3002,
       },
     },
+    {
+      name: 'nest-main',
+      script: 'dist/apps/main/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
   ],
 };
